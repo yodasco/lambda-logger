9604fa11d45a614dda1fa8adc69c463c5bd64ec3
Ran Tavory <rantav@gmail.com>
2017-06-04 09:43:25 +0300

    Refactor: Move the logic of AWS event parsing to the parse

--- lib/parser/parser.go
+++ lib/parser/parser.go
@@ -1,15 +1,39 @@
 package parser
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
 	"encoding/json"
+	"io"
 	"time"
 
 	"github.com/yodasco/lambda-logger/lib/types"
 )
 
+// AwsLambdaLogGroupEvent is used to decompose the incoming logs events.
+// This is the structure of an incoming log event
+// Incoming event looks like:
+// {"awslogs":{"data":"===base64 zipped json array ==="}}
+type AwsLambdaLogGroupEvent struct {
+	AwsLogs struct {
+		Data string `json:"data"`
+	} `json:"awslogs"`
+}
+
 // Parse log lines from AWS Cloudwatch logs log groups.
-func Parse(bytes []byte) (events types.CloudwatchLogEvents, err error) {
-	err = json.Unmarshal(bytes, &events)
+func Parse(event json.RawMessage) (events types.CloudwatchLogEvents, err error) {
+	var lambdaEvent AwsLambdaLogGroupEvent
+	if err = json.Unmarshal(event, &lambdaEvent); err != nil {
+		return
+	}
+
+	decoded, err := decode(lambdaEvent.AwsLogs.Data)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(decoded, &events)
 	if err != nil {
 		return
 	}
@@ -43,4 +67,22 @@ func Parse(bytes []byte) (events types.CloudwatchLogEvents, err error) {
 func fromUnixMilli(ms int64) time.Time {
 	return time.Unix(ms/1000, 0).Add(
 		time.Duration(ms%1000) * time.Millisecond)
+}
+
+// Decodes a base64 gzipped string
+// That's how AWS log evnets show up
+func decode(b64z string) ([]byte, error) {
+	data, err := base64.StdEncoding.DecodeString(b64z)
+	if err != nil {
+		return nil, err
+	}
+
+	reader, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, reader)
+	return buf.Bytes(), err
 }
